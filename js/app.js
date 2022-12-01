4167ff0d04ffcabe59d38f7d64378a263526223c
Korèn <kpapazian@edenschool.fr>
2022-12-01 11:48:24 +0100

    end

--- js/app.js
+++ js/app.js
@@ -11,22 +11,30 @@ let ul = document.getElementById("ul");
 function ready () {
     let words = httpRequest.response.split("\r\n");
     input.addEventListener("input", () => {
+        console.log(true);
         recherche(words);
     });
 }
 
 function recherche (words) {
+    ul.innerHTML = "";
+    ul.classList.remove("visible");
+    ul.classList.add("hidden");
+    let filtre = words.filter(el => el.startsWith(input.value)).slice(0, 10);
+    console.log(filtre);
     if (input.value.length >= 3) {
-        let filtre = words.filter(el => el.startsWith(input.value)).slice(0, 10);
         for (let i = 0; i < filtre.length; i++) {
+            ul.classList.remove("hidden");
+            ul.classList.add("visible");
             let new_li = document.createElement("li");
             ul.appendChild(new_li);
-            new_li.textContent = filtre[i];
+
             new_li.addEventListener("click", () => {
                 input.value = filtre[i];
+                recherche(words);
             });
+            
+            new_li.textContent = filtre[i];
         }
-    } else {
-        ul.innerHTML = "";
     }
 }
