a7dc6072683a0bfa0ad6c03b0650131bac64a520
Korèn <kpapazian@edenschool.fr>
2022-12-01 09:42:52 +0100

    end

--- js/app.js
+++ js/app.js
@@ -9,7 +9,6 @@ let input = document.getElementById("input");
 let ul = document.getElementById("ul");
 
 function ready () {
-    console.log(httpRequest.response.split("\r\n"));
     let words = httpRequest.response.split("\r\n");
     input.addEventListener("input", () => {
         recherche(words);
@@ -18,22 +17,16 @@ function ready () {
 
 function recherche (words) {
     if (input.value.length >= 3) {
-        let new_li = document.createElement("li");
-        new_li.textContent = words.filter(el => el.startsWith(input.value));
-        ul.appendChild(new_li);
-        console.log(words.filter(el => el.startsWith(input.value)));
+        let filtre = words.filter(el => el.startsWith(input.value)).slice(0, 10);
+        for (let i = 0; i < filtre.length; i++) {
+            let new_li = document.createElement("li");
+            ul.appendChild(new_li);
+            new_li.textContent = filtre[i];
+            new_li.addEventListener("click", () => {
+                input.value = filtre[i];
+            });
+        }
+    } else {
+        ul.innerHTML = "";
     }
 }
-
-
-
-// if (input.value[3] == word_eclater[0]) {
-//     let new_li = document.createElement("li");
-//     new_li.classList.add("li");
-//     new_li.textContent = word_eclater.join("");
-//     ul.appendChild(new_li);
-
-//     new_li.addEventListener("click", () => {
-//         input.value = word_eclater.join("");
-//     });
-// }
